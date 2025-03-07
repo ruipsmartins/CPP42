66f152fcaae3850cec214ba7aaf99fb4dc7763f7
Rui Martins <rui_pedru@hotmail.com>
2025-03-07 17:54:40 +0000

    Add megaphone project files with Makefile, header, and implementation

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 17:42:38 by ruidos-s          #+#    #+#             */
+/*   Updated: 2025/03/07 17:49:52 by ruidos-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "megaphone.hpp"
+
+int main (int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	(void)argv;
+
+	
+	return 0;
+}
