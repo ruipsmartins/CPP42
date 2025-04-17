0fb47f013d6c9aec060dbccc20cd4d01d6210caf
Rui Martins <rui_pedru@hotmail.com>
2025-04-17 10:45:00 +0100

    inicio cpp

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,29 +6,29 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 17:42:38 by ruidos-s          #+#    #+#             */
-/*   Updated: 2025/03/09 12:43:53 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2025/04/17 10:44:37 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "megaphone.hpp"
 
-int main (int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	if (argc < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-
-	else
+	if (argc == 1)
 	{
-		for(int i = 1; argv[i]; i++)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+
+	for (int i = 1; argv[i]; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
 		{
-			for(int j = 0; argv[i][j]; j++)
-			{
-				std::cout << (char)toupper(argv[i][j]); 
-			}
+			std::cout << (char)toupper(argv[i][j]);
 		}
 	}
-	
+
 	std::cout << std::endl;
 
-	return 0;
+	return (0);
 }
