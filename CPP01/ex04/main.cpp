bf2484a307ee6318c95cec911d9c3553be548d7e
Rui Martins <rui_pedru@hotmail.com>
2025-05-06 15:10:51 +0100

    ex04 upload

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,12 +6,36 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:26:13 by ruidos-s          #+#    #+#             */
-/*   Updated: 2025/05/05 18:28:15 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2025/05/06 12:27:42 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+#include <fstream>
+
+
+
+void replaceInFile(std::ifstream &inFile, std::ofstream &outFile, const std::string s1, const std::string s2)
+{
+	std::string line;
+
+	while(std::getline(inFile, line))
+	{
+		size_t pos = 0;
+
+		while ((pos = line.find(s1, pos)) != std::string::npos)
+		{
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+			pos += s2.length();
+		}
+		outFile << line;
+		if (!inFile.eof())
+			outFile << std::endl;
+	}
+}
+
 
 int main(int argc, char **argv)
 {
@@ -20,10 +44,38 @@ int main(int argc, char **argv)
 		std::cout << "Usage: ./replace <filename> <s1> <s2>" << std::endl;
 		return (1);
 	}
+
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
 	
-	std::cout << argv[1] << std::endl;
-	std::cout << argv[2] << std::endl;
-	std::cout << argv[3] << std::endl;
+	if (s1.empty() || s2.empty())
+	{
+		std::cerr << "s1 or s2 cant be empty" << std::endl;
+		return 1;
+	}
+	
+	std::ifstream inFile(filename.c_str());
+	if (!inFile.is_open())
+	{
+		std::cerr << "Error: could not open input file" << std::endl;
+		return (1);
+	}
+
+	std::ofstream outFile((filename + ".replace").c_str());
+	if (!outFile.is_open())
+	{
+		std::cerr << "Error: could not open output file" << std::endl;
+		inFile.close();
+		return (1);
+	}
+	
+	
+	replaceInFile(inFile, outFile,s1, s2);
+
+
+	inFile.close();
+	outFile.close();
 
 	return (0);
 }
