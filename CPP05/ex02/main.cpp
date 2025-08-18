1b3929021fc12f5373ccab7d7821fd620ecad551
Rui Martins <ruidos-s@student.42lisboa.com>
2025-08-18 12:16:01 +0100

    update main

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -8,46 +8,53 @@
 int main()
 {
 	// std::srand(static_cast<unsigned int>(std::time(NULL)));
+	try
+	{
+		Bureaucrat boss("Boss", 1);
+		Bureaucrat rui("Rui", 10);
+		Bureaucrat jann("Jann", 137);
+		Bureaucrat ze("Ze", 140);
+		Bureaucrat intern("Intern", 150);
 
-	Bureaucrat boss("Boss", 1);
-	Bureaucrat intern("Intern", 150);
-	Bureaucrat jann("Jann", 137);
-	Bureaucrat ze("Ze", 140);
-	Bureaucrat rui("Rui", 10);
+		 { // ----- SHRUBBERY -----
+			 std::cout << "\n=== SHRUBBERY ===\n";
+			 ShrubberyCreationForm shrub("home");
 
-	 { // ----- SHRUBBERY -----
-		 std::cout << "\n=== SHRUBBERY ===\n";
-		 ShrubberyCreationForm shrub("home");
+			 boss.executeForm(shrub);   // tentar executar sem assinar (vai falhar)
+			 ze.signForm(shrub);        // Ze (140) assina (sign <= 145)
+			 jann.executeForm(shrub);   // Jann (137) executa (exec == 137)
+		 }
 
-		 boss.executeForm(shrub);   // tentar executar sem assinar (deve falhar)
-		 ze.signForm(shrub);        // Ze (140) assina (sign <= 145)
-		 jann.executeForm(shrub);   // Jann (137) executa (exec == 137)
-	 }
+		/* { // ----- ROBOTOMY -----
+			std::cout << "\n=== ROBOTOMY ===\n";
+			RobotomyRequestForm robot("Bender");
 
-	/*  { // ----- ROBOTOMY -----
-		 std::cout << "\n=== ROBOTOMY ===\n";
-		 RobotomyRequestForm robot("Bender");
+			boss.signForm(robot);		 // Boss assina
+			for (int i = 0; i < 10; ++i) // executa várias vezes para ver sucesso/falha 50/50
+				boss.executeForm(robot);
+		} */
 
-		 boss.signForm(robot);       // Boss assina
-		 for (int i = 0; i < 10; ++i) // executa várias vezes para veres sucesso/falha 50/50
-			 boss.executeForm(robot);
-	 } */
+		/*  { // ----- PRESIDENTIAL PARDON -----
+			 std::cout << "\n=== PRESIDENTIAL PARDON ===\n";
+			 PresidentialPardonForm pardon("Arthur Dent");
 
-	/*  { // ----- PRESIDENTIAL PARDON -----
-		 std::cout << "\n=== PRESIDENTIAL PARDON ===\n";
-		 PresidentialPardonForm pardon("Arthur Dent");
+			 boss.signForm(pardon);     // Boss assina (sign = 25)
+			 rui.executeForm(pardon);   // Rui (10) tenta executar — vai falhar (exec = 5)
+			 boss.executeForm(pardon);  // Boss executa (sucesso)
+		 } */
 
-		 boss.signForm(pardon);     // Boss assina (sign = 25)
-		 rui.executeForm(pardon);   // Rui (10) tenta executar — deve falhar (exec = 5)
-		 boss.executeForm(pardon);  // Boss executa (sucesso)
-	 } */
+		/* { // ----- INTERN TRY SIGN -----
+			std::cout << "\n=== INTERN TRY SIGN ===\n";
+			ShrubberyCreationForm shrub2("garden");
+			intern.signForm(shrub2);   // Intern (150) tenta assinar — vai falhar
+		} */
 
-	 /* { // ----- INTERN TRY SIGN -----
-		 std::cout << "\n=== INTERN TRY SIGN ===\n";
-		 ShrubberyCreationForm shrub2("garden");
-		 intern.signForm(shrub2);   // Intern (150) tenta assinar — deve falhar
-	 } */
+		std::cout << "\n--- FIM DOS TESTES ---\n";
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
-	std::cout << "\n--- FIM DOS TESTES ---\n";
 	return 0;
 }
